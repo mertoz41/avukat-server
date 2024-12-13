b895924a8b17d727092f371d03abb42a9d3c05a1
mert <mertozka41@gmail.com>
2024-12-14 01:54:45 +0300

    initial commit

---
+++ server/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Lawyer(AbstractUser):
+    email = models.EmailField(blank=False, max_length=255, verbose_name="email")
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
